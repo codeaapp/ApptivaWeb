08847afee90b09b0f706f980417113972f821a19
Luis <jluis@apptivaweb.com>
2017-11-23 18:28:28 -0500

    dos

--- index.php
+++ index.php
@@ -1,13 +1,16 @@
 <!DOCTYPE html>
 	<html>
 	<head>
-		<title>HOLA SOY GIT HUB JAJAJAJJAJAJAJJAJAJAUJAUAUUAUAAJ</title>
+		<title>USO DEL PHP</title>
 	</head>
 
 	<body>
 
 
-		<h1>hola soy JUAUAUAJJAJUAJJAUAUUAJAUAUUAJJ JUAJAUUA</h1>
+		<h1>USO DE PHP</h1>
+		<?php for($x=1;$x>=100;$x++) ?>
+		<h1><?php echo $x ?></h1>
+		<?php endfor; ?>
 	
 	</body>
 	</html>	
